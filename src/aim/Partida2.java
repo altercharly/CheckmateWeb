10e80bf603d4660ed51c59675476130320bc1ee0
López Carlos <carloslopez2711@gmail.com>
2015-12-10 15:59:34 -0300

    Change getAttribute for getParameter

--- src/aim/Partida2.java
+++ src/aim/Partida2.java
@@ -44,24 +44,24 @@ public class Partida2 extends HttpServlet {
 		HttpSession session = request.getSession();
 		Usuario jug1 = (Usuario) session.getAttribute("user");
 		String dni=request.getParameter("dnicont");
-		char pieza = ((request.getAttribute("nombreficha")).toLowerCase().charAt(0));
-		char posix = ((request.getAttribute("posx")).toLowerCase().charAt(0));
-		char nposix = ((request.getAttribute("newposx")).toLowerCase().charAt(0));
-		int posiy = Integer.parseInt(request.getAttribute("posy"));
-		int nposiy = Integer.parseInt(request.getAttribute("newposy"));
+		char pieza = ((request.getParameter("nombreficha")).toLowerCase().charAt(0));
+		char posix = ((request.getParameter("posx")).toLowerCase().charAt(0));
+		char nposix = ((request.getParameter("newposx")).toLowerCase().charAt(0));
+		int posiy = Integer.parseInt(request.getParameter("posy"));
+		int nposiy = Integer.parseInt(request.getParameter("newposy"));
 		//response.getWriter().append(email).append(": ").append(pass);
 		int dni2=Integer.parseInt(dni);
 		negocio.ControladorLogin cl= new ControladorLogin();
 		Usuario jug2 = cl.getUsuarioByDni(dni2);
 		Partida p = new Partida();
-		p = request.getAttribute("p");
+		p = request.getParameter("p");
 		int idpart = p.getid();
 		cl.movimientovalido(pieza, posix, posiy, nposix, nposiy, jug1, jug2, idpart);
 		
 			session.setAttribute("user", jug1);
 			session.setAttribute("jug2", jug2);
 			session.setAttribute("partida", p);
-			response.sendRedirect("Partida.jsp");
+			response.sendRedirect("/Partida.jsp");
 		
 	}
 	
